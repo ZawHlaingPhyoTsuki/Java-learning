c86a65d546a7333899b7e5cc30412dc0ee18f02f
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-11-07 20:56:18 +0700

    feat[blog]: create category repository

---
+++ blog/src/main/java/com/devtiro/blog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.devtiro.blog.repositories;
+
+import com.devtiro.blog.domain.entities.Category;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository()
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
