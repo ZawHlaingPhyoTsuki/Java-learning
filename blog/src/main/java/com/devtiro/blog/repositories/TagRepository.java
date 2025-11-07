74e1e2619188e0ef5f68b4eef69046ad15bfe1f6
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-11-07 20:57:44 +0700

    feat[blog]: create tag repository

--- blog/src/main/java/com/devtiro/blog/repositories/TagRepository.java
+++ blog/src/main/java/com/devtiro/blog/repositories/TagRepository.java
@@ -1,11 +1,11 @@
 package com.devtiro.blog.repositories;
 
-import com.devtiro.blog.domain.entities.Category;
+import com.devtiro.blog.domain.entities.Tag;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, UUID> {
+public interface TagRepository extends JpaRepository<Tag, UUID> {
 
 }
