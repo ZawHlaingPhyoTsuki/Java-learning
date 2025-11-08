1c9dfd8b523b4441b12ed0df9ded65c2408dfc3b
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-11-08 15:28:43 +0700

    feat[blog]: Error Handling

---
+++ blog/src/main/java/com/devtiro/blog/domain/dtos/ApiErrorResponse.java
@@ -0,0 +1,26 @@
+package com.devtiro.blog.domain.dtos;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiErrorResponse {
+  private int status;
+  private String message;
+  private List<FieldError> errors;
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class FieldError {
+    private String field;
+    private String error;
+  }
+}
