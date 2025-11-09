bcc7ed5ba5a1995ba42e36eb2b56f3a595928ecd
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-11-09 15:04:50 +0700

    feat[blog]: Add Spring Security

--- blog/src/main/java/com/devtiro/blog/repositories/UserRepository.java
+++ blog/src/main/java/com/devtiro/blog/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devtiro.blog.repositories;
 
 import com.devtiro.blog.domain.entities.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+  Optional<User> findByEmail(String email);
 }
